d1f92b8222878880799fa2eba3581af583ef9672
wenweihu86 <wenweihu86@163.com>
2018-02-23 16:29:59 +0800

    增加redis普通模式的封装

--- src/main/java/com/github/wenweihu86/commmons/util/RedisDao.java
+++ src/main/java/com/github/wenweihu86/commmons/util/RedisDao.java
@@ -8,62 +8,43 @@ import redis.clients.jedis.*;
 import redis.clients.jedis.params.sortedset.ZAddParams;
 import redis.clients.jedis.params.sortedset.ZIncrByParams;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 /**
- * 对redis集群模式的使用封装，基于jedis
+ * 对redis的使用封装，基于jedis
  */
+
 @Setter
 @Getter
 public class RedisDao {
     private static final Logger LOG = LoggerFactory.getLogger(RedisDao.class);
 
-    // 集群节点集合
-    private Set<HostAndPort> nodes;
-    // 连接超时时间，单位毫秒
-    private int connectionTimeoutMs = 500;
-    // 读写超时时间，单位毫秒
-    private int readWriteTimeoutMs = 1000;
-    // 最大重试次数，包括首次请求
-    private int maxRetryCount = 3;
     // 连接池配置，同apache commons pool配置
     private JedisPoolConfig redisPoolConfig = new JedisPoolConfig();
-    // jedis cluster对象
-    private JedisCluster redisCluster;
+    private String host;
+    private Integer port;
+    // 连接+读写超时时间，单位毫秒
+    private Integer timeoutMs = 500;
+    private JedisPool redisPool;
 
-    /**
-     * 初始化连接
-     */
     public void init() {
-        redisCluster = new JedisCluster(nodes, connectionTimeoutMs,
-                readWriteTimeoutMs, maxRetryCount, redisPoolConfig);
-        Map<String, JedisPool> clusterNodes = redisCluster.getClusterNodes();
-        if (clusterNodes.keySet().size() == 0) {
-            LOG.warn("redis cluster node size == 0");
-            throw new RuntimeException("redis cluster node size == 0");
-        }
+        redisPool = new JedisPool(redisPoolConfig, host, port, timeoutMs);
     }
 
-    /**
-     * 关闭连接
-     */
     public void close() {
-        if (redisCluster != null) {
-            try {
-                redisCluster.close();
-            } catch (IOException ex) {
-                LOG.warn("close redis cluster exception={}", ex);
-            }
+        if (redisPool != null) {
+            redisPool.close();
         }
     }
 
     // 普通kv操作
 
     public String set(final String key, final String value) {
-        return redisCluster.set(key, value);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.set(key, value);
+        }
     }
 
     /**
@@ -77,185 +58,271 @@ public class RedisDao {
      */
     public String set(final String key, final String value,
                       final String nxxx, final String expx, final long time) {
-        return redisCluster.set(key, value, nxxx, expx, time);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.set(key, value, nxxx, expx, time);
+        }
     }
 
     public String get(final String key) {
-        return redisCluster.get(key);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.get(key);
+        }
     }
 
     public Long del(final String key) {
-        return redisCluster.del(key);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.del(key);
+        }
     }
 
     // hash操作
 
     public Long hset(final String key, final String field, final String value) {
-        return redisCluster.hset(key, field, value);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.hset(key, field, value);
+        }
     }
 
     public String hget(final String key, final String field) {
-        return redisCluster.hget(key, field);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.hget(key, field);
+        }
     }
 
     public String hmset(final String key, final Map<String, String> hash) {
-        return redisCluster.hmset(key, hash);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.hmset(key, hash);
+        }
     }
 
     public List<String> hmget(final String key, final String... fields) {
-        return redisCluster.hmget(key, fields);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.hmget(key, fields);
+        }
     }
 
     public Map<String, String> hgetAll(final String key) {
-        return redisCluster.hgetAll(key);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.hgetAll(key);
+        }
     }
 
     // list操作
 
     public Long lpush(final String key, final String... values) {
-        return redisCluster.lpush(key, values);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.lpush(key);
+        }
     }
 
     public Long llen(final String key) {
-        return redisCluster.llen(key);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.llen(key);
+        }
     }
 
     public List<String> lrange(final String key, final long start, final long end) {
-        return redisCluster.lrange(key, start, end);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.lrange(key, start, end);
+        }
     }
 
     public String lindex(final String key, final long index) {
-        return redisCluster.lindex(key, index);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.lindex(key, index);
+        }
     }
 
     public String lset(final String key, final long index, final String value) {
-        return redisCluster.lset(key, index, value);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.lset(key, index, value);
+        }
     }
 
     public Long lrem(final String key, final long count, final String value) {
-        return redisCluster.lrem(key, count, value);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.lrem(key, count, value);
+        }
     }
 
     public String lpop(final String key) {
-        return redisCluster.lpop(key);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.lpop(key);
+        }
     }
 
     public String rpop(final String key) {
-        return redisCluster.rpop(key);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.rpop(key);
+        }
     }
 
     // set操作
 
     public Long sadd(final String key, final String... member) {
-        return redisCluster.sadd(key, member);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.sadd(key, member);
+        }
     }
 
     public Set<String> smembers(final String key) {
-        return redisCluster.smembers(key);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.smembers(key);
+        }
     }
 
     public Long srem(final String key, final String... member) {
-        return redisCluster.srem(key, member);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.srem(key, member);
+        }
     }
 
     public String spop(final String key) {
-        return redisCluster.spop(key);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.spop(key);
+        }
     }
 
     public Set<String> spop(final String key, final long count) {
-        return redisCluster.spop(key, count);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.spop(key, count);
+        }
     }
 
     public Long scard(final String key) {
-        return redisCluster.scard(key);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.scard(key);
+        }
     }
 
     public Boolean sismember(final String key, final String member) {
-        return redisCluster.sismember(key, member);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.sismember(key, member);
+        }
     }
 
     public String srandmember(final String key) {
-        return redisCluster.srandmember(key);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.srandmember(key);
+        }
     }
 
     public List<String> srandmember(final String key, final int count) {
-        return redisCluster.srandmember(key, count);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.srandmember(key, count);
+        }
     }
 
     public Long strlen(final String key) {
-        return redisCluster.strlen(key);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.strlen(key);
+        }
     }
 
     // sorted set操作
 
     public Long zadd(final String key, final double score, final String member) {
-        return redisCluster.zadd(key, score, member);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.zadd(key, score, member);
+        }
     }
 
     public Long zadd(final String key, final double score,
                      final String member, final ZAddParams params) {
-        return redisCluster.zadd(key, score, member, params);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.zadd(key, score, member, params);
+        }
     }
 
     public Long zadd(final String key, final Map<String, Double> scoreMembers) {
-        return redisCluster.zadd(key, scoreMembers);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.zadd(key, scoreMembers);
+        }
     }
 
     public Long zadd(final String key, final Map<String, Double> scoreMembers, final ZAddParams params) {
-        return redisCluster.zadd(key, scoreMembers, params);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.zadd(key, scoreMembers, params);
+        }
     }
 
     public Set<String> zrange(final String key, final long start, final long end) {
-        return redisCluster.zrange(key, start, end);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.zrange(key, start, end);
+        }
     }
 
     public Long zrem(final String key, final String... member) {
-        return redisCluster.zrem(key, member);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.zrem(key, member);
+        }
     }
 
     public Double zincrby(final String key, final double score, final String member) {
-        return redisCluster.zincrby(key, score, member);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.zincrby(key, score, member);
+        }
     }
 
     public Double zincrby(final String key, final double score,
                           final String member, final ZIncrByParams params) {
-        return redisCluster.zincrby(key, score, member, params);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.zincrby(key, score, member, params);
+        }
     }
 
     public Long zrank(String key, String member) {
-        return redisCluster.zrank(key, member);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.zrank(key, member);
+        }
     }
 
     public Long zrevrank(String key, String member) {
-        return redisCluster.zrevrank(key, member);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.zrevrank(key, member);
+        }
     }
 
     public Set<String> zrevrange(String key, long start, long end) {
-        return redisCluster.zrevrange(key, start, end);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.zrevrange(key, start, end);
+        }
     }
 
     public Set<Tuple> zrangeWithScores(String key, long start, long end) {
-        return redisCluster.zrangeWithScores(key, start, end);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.zrangeWithScores(key, start, end);
+        }
     }
 
     public Set<Tuple> zrevrangeWithScores(String key, long start, long end) {
-        return redisCluster.zrevrangeWithScores(key, start, end);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.zrevrangeWithScores(key, start, end);
+        }
     }
 
     public Long zcard(String key) {
-        return redisCluster.zcard(key);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.zcard(key);
+        }
     }
 
     public Double zscore(String key, String member) {
-        return redisCluster.zscore(key, member);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.zscore(key, member);
+        }
     }
 
     public List<String> sort(String key) {
-        return redisCluster.sort(key);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.sort(key);
+        }
     }
 
     public List<String> sort(String key, SortingParams sortingParameters) {
-        return redisCluster.sort(key, sortingParameters);
+        try (Jedis jedis = redisPool.getResource()) {
+            return jedis.sort(key, sortingParameters);
+        }
     }
 
 }
--- src/test/java/com/github/wenweihu86/commons/util/RedisClusterDaoTest.java
+++ src/test/java/com/github/wenweihu86/commons/util/RedisClusterDaoTest.java
@@ -1,7 +1,6 @@
 package com.github.wenweihu86.commons.util;
 
 import com.github.wenweihu86.commmons.util.RedisClusterDao;
-import com.github.wenweihu86.commmons.util.RedisDao;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -11,14 +10,20 @@ import redis.clients.jedis.HostAndPort;
 import java.util.HashSet;
 import java.util.Set;
 
-public class RedisDaoTest {
-    private RedisDao redisDao;
+public class RedisClusterDaoTest {
+    private RedisClusterDao redisDao;
 
     @Before
     public void setUp() {
-        redisDao = new RedisDao();
-        redisDao.setHost("127.0.0.1");
-        redisDao.setPort(6379);
+        Set<HostAndPort> nodes = new HashSet<>();
+        nodes.add(new HostAndPort("127.0.0.1", 30000));
+        nodes.add(new HostAndPort("127.0.0.1", 30001));
+        nodes.add(new HostAndPort("127.0.0.1", 30002));
+        nodes.add(new HostAndPort("127.0.0.1", 30003));
+        nodes.add(new HostAndPort("127.0.0.1", 30004));
+        nodes.add(new HostAndPort("127.0.0.1", 30005));
+        redisDao = new RedisClusterDao();
+        redisDao.setNodes(nodes);
         redisDao.init();
     }
 
